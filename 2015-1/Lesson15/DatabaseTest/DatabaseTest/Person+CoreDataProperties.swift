17c38cc580639646eee368f8133dee966b8d1d44
Dal Rupnik <legoless@gmail.com>
2015-12-01 09:53:08 +0100

    Added core data

---
+++ 2015-1/Lesson15/DatabaseTest/DatabaseTest/Person+CoreDataProperties.swift
@@ -0,0 +1,21 @@
+//
+//  Person+CoreDataProperties.swift
+//  DatabaseTest
+//
+//  Created by Dal Rupnik on 30/11/15.
+//  Copyright © 2015 Unified Sense. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension Person {
+
+    @NSManaged var name: String?
+    @NSManaged var birthday: NSDate?
+    @NSManaged var cars: NSSet?
+
+}
